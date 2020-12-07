e8fd32dd8b53db21da5415ed27ff656bb2d07aa8
Huelder <huelderantuness@gmail.com>
2020-12-06 23:00:28 -0300

    chapter 5: adding a sphere

--- main.cpp
+++ main.cpp
@@ -4,12 +4,23 @@
 
 #include <iostream>
 
+
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
-
 
 int main() {
     // image
@@ -36,7 +47,7 @@ int main() {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto u = double(i) / (double(image_width) - 1);
-            auto v = double(j) / (double(image_width) - 1);
+            auto v = double(j) / (double(image_height) - 1);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
             color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
